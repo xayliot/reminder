25847866527481787abd03356fa693d11809f0f7
xayliot <megaponch2004@gmail.com>
2024-12-21 22:39:02 +0300

    -'

---
+++ simple-chat/webpack.config.js
@@ -0,0 +1,77 @@
+'use strict';
+
+const path = require('path');
+
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+
+const SRC_PATH = path.resolve(__dirname, 'src');
+const BUILD_PATH = path.resolve(__dirname, 'build');
+
+module.exports = {
+    context: SRC_PATH,
+    entry: {
+        index: './index.js',
+        chats: './components/chats/chats.js',
+        header: './components/header/header.js',
+    },
+    output: {
+        path: BUILD_PATH,
+        filename: '[name].bundle.js'
+    },
+    module: {
+        strictExportPresence: true,
+        rules: [
+            {
+                test: /\.js$/,
+                include: SRC_PATH,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        },
+                    },
+                ],
+            },
+            {
+                test: /shadow\.css$/,
+                include: SRC_PATH,
+                use: [
+                    {
+                        loader: 'css-loader'
+                    },
+                ],
+            },
+            {
+                test: /\.css$/,
+                include: SRC_PATH,
+                use: [
+                    {
+                        loader: MiniCSSExtractPlugin.loader,
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            filename: 'index.html',
+            template: './index.html',
+            chunks: ['index', 'header']
+                    }),
+                    new HTMLWebpackPlugin({
+                        filename: 'chats.html',
+                        template: './components/chats/chats.html',
+                        chunks: ['chats']
+                    }),
+                    new MiniCSSExtractPlugin({
+                        filename: '[name].css',
+                    }),
+                 ]
+             };
+            
